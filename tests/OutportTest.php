e5f5865e71a2ae195d0611462b143643212ae7c4
Ahmet Ozisik <ozisikahmet@gmail.com>
2016-11-05 17:59:08 +0300

    added a functional test

--- tests/OutportTest.php
+++ tests/OutportTest.php
@@ -6,6 +6,39 @@ class OutportTest extends TestCase
 {
     public function testItMigratesDataCorrectly()
     {
-        $outport = new Outport();
+        $books = [
+            [
+                'title' => 'Pride and Prejudice',
+                'author' => 'Jane Austen'
+            ],
+            [
+                'title' => 'Oliver Twist',
+                'author' => 'Charles Dickens'
+            ],
+            [
+                'title' => 'The Fault in Our Stars',
+                'author' => 'John Green'
+            ]
+        ];
+
+        $path = (new Outport())->table('books', collect($books))->go();
+        $this->assertFileExists($path);
+
+        Config::set('database.connections.outport-test', [
+            'driver' => 'sqlite',
+            'database' => $path,
+            'prefix' => '',
+        ]);
+
+        $readFromSQLite = DB::connection('outport-test')
+            ->table('books')
+            ->select('title', 'author')
+            ->get()
+            ->map(function ($item) {
+                return (array)$item;
+            })->toArray();
+
+        $this->assertEquals($books, $readFromSQLite);
+        unlink($path);
     }
 }
