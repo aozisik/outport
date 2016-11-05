0e399387d154ba518a81ebf95748d48022825a54
Ahmet Ozisik <ozisikahmet@gmail.com>
2016-11-05 17:49:06 +0300

    started writing tests

---
+++ tests/OutportTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Aozisik\Outport\Outport;
+
+class OutportTest extends TestCase
+{
+    public function testItMigratesDataCorrectly()
+    {
+        $outport = new Outport();
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../vendor/laravel/laravel/bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+
+}
